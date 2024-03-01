49f2243a15161b2e000791c265f6274c24027bfd
Jacob Van Dyke <jacobvandyke194@gmail.com>
2024-02-29 22:44:24 -0700

    cleaned up some things

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-//create arr that contains words I would like console animation to display
-//create blinking underscore element
-//loop through each letter of word and input into DOM BEFORE underscore
-//once word is complete, create call function that loops through word and removes letter backwords
-
 const introWord = "Hello! Welcome to my profile page!";
 const introPhrase = introWord.split('');
 
-
-
 let consWords = document.getElementById("consWords");
-let consPhrase = document.getElementById("consPhrase");
 let consUnderscore = document.getElementById("consUnderscore");
+let personalInfo = document.getElementById("personalInfo");
 
 let index = 0;
 let isBool = true;
 
-setInterval(underscoreBlink, 500)
 
 function underscoreBlink() {
   if (isBool) {
@@ -27,8 +19,6 @@ function underscoreBlink() {
   }
 }
 
-//
-
 function introPhraseThing(){
 
   if(introPhrase.length > 0){
@@ -36,16 +26,16 @@ function introPhraseThing(){
     let h1 = document.createTextNode(character);
     h1.textContent = character;
     
-
     consWords.appendChild(h1);
 
     index++;
    
-
     setTimeout(introPhraseThing, 250);
+  } else if(introPhrase.length == 0){
+    //call future func here for personal info/project information
   }
 }
 
-introPhraseThing();
 
-//for loop that places each letter in array in it's own <span> with 'hidden' class that gets removed one by one? 
+introPhraseThing();
+setInterval(underscoreBlink, 500)
