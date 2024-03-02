e755bc3b6312e56f344e67e8cb367795eaa3ac75
Jacob Van Dyke <jacobvandyke194@gmail.com>
2024-03-02 16:48:02 -0700

    added about me section and coding projects section

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-//1. variable that contains string of words I want it to spell out
-//2. create a function that splits string into an array
-//3. loop over the array and append each letter to the DOM
-//4. use setInterval() to call function every 1 second
-
-
-const introPhrase = "Hello! Welcome to my portfolio page!"
-const consDomInput = document.getElementById('consWords');
+const introPhrase = "Hello! Welcome to my portfolio page!";
+const consDomInput = document.getElementById("consWords");
 let phraseIndex = 0;
 
-
-function consoleWriter(phrase, domInput){
-  let phraseArray = phrase.split('');
+function consoleWriter(phrase, domInput, interval, nextFunc) {
+  let phraseArray = phrase.split("");
   phraseArray.push(0);
-  const paragraphTag = document.createElement('p');
+  const paragraphTag = document.createElement("p");
 
-  if(phraseArray[phraseIndex] === 0){
+  if (phraseArray[phraseIndex] === 0) {
+    //inputs flag to stop interval
     phraseArray.pop();
-    clearInterval(consoleWriterInterval);
-  }
-  else if(phraseArray.length > 0){
-      if(phraseArray[phraseIndex] === " "){
-      paragraphTag.innerHTML = '&nbsp;';
+    clearInterval(interval);
+    //calls next function
+    nextFunc();
+  } else if (phraseArray.length > 0) {
+    if (phraseArray[phraseIndex] === " ") {
+      //checks for spaces to be properly input into DOM
+      paragraphTag.innerHTML = "&nbsp;";
       domInput.appendChild(paragraphTag);
-      phraseIndex++
-    }else{
-    paragraphTag.textContent = phraseArray[phraseIndex];
-    domInput.appendChild(paragraphTag);
-    phraseIndex++;
-  } 
-}
+      phraseIndex++;
+    } else {
+      //Inputs every other character into DOM
+      paragraphTag.textContent = phraseArray[phraseIndex];
+      domInput.appendChild(paragraphTag);
+      phraseIndex++;
+    }
+  }
 }
 
-const consoleWriterInterval = setInterval(function(){consoleWriter(introPhrase, consDomInput)}, 250);
+//calls first consolewriter. The writer takes 4 params the phrase, where it is input 
+//in the DOM and the name of the interval calling it in this case itself, finally the next function
+//to be called after the console writer is completed
+const introConsoleWriterInterval = setInterval(function () {
+  consoleWriter(introPhrase, consDomInput, introConsoleWriterInterval, nextFunction);
+}, 200);
+
+function nextFunction(){
+
+}
+//About Me Header Console Scroll
+
+
